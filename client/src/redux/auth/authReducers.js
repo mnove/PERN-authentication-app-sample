ea933eace3ea80eb6388f1ede0955f7614222c8b
mnove <novelli3000@gmail.com>
2021-04-27 21:28:41 +0200

    minor change to code

--- client/src/redux/auth/authReducers.js
+++ client/src/redux/auth/authReducers.js
@@ -1,55 +1,55 @@
 import {
-    LOGIN_REQUEST,
-    LOGIN_SUCCESS,
-    LOGIN_FAILURE,
-    LOGOUT_REQUEST,
-    LOGOUT_SUCCESS
-  } from "./authTypes";
-  
-  const initialState = {
-    loading: false,
-    isAuthenticated: false,
-    error: ""
-  };
-  
-  const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case LOGIN_REQUEST:
-        return {
-          ...state,
-          loading: true,
-        };
-  
-      case LOGIN_SUCCESS:
-        return {
-            ...state,
-            loading: false,
-            isAuthenticated: true,
-        };
-  
-      case LOGIN_FAILURE:
-        return {
-            loading: false,
-            isAuthenticated: false,
-            error: action.payload,
-        };
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT_REQUEST,
+  LOGOUT_SUCCESS,
+} from "./authTypes";
 
-        case LOGOUT_REQUEST:
-        return {
-            ...state,
-            loading: true,
-        };
+const initialState = {
+  loading: false,
+  isAuthenticated: false,
+  error: "",
+};
 
-        case LOGOUT_SUCCESS:
-        return {
-            ...state,
-            loading: false,
-            isAuthenticated: false
-        };
-  
-      default:
-        return state;
-    }
-  };
-  
-  export default authReducer;
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+      };
+
+    case LOGIN_FAILURE:
+      return {
+        loading: false,
+        isAuthenticated: false,
+        error: action.payload,
+      };
+
+    case LOGOUT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
